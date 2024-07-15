579f73ed8708ecfff61d6751e27816ff1005b76e
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-15 17:35:33 +0200

    Completato bonus 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
-const myUl = document.querySelector('ul');
+const boxContainer = document.querySelector('div.box-container');
 
 for (let i = 1; i <= 100; i++) {
     let testo;
+    let className;
 
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBuzz');
-        testo = 'FizzBuzz'
+        testo = 'FizzBuzz';
+        className = 'fizzbuzz';
     } 
 
     else if (i % 3 == 0) {
         console.log('fizz');
-        testo = "fizz"
+        testo = 'fizz';
+        className = 'fizz';
     }
 
     else if (i % 5 == 0) {
         console.log('buzz');
-        testo = "buzz"
+        testo = 'buzz';
+        className = 'buzz';
     }
 
     else {
@@ -23,8 +27,32 @@ for (let i = 1; i <= 100; i++) {
         testo = i;
     }
 
-    const newLi = document.createElement('li');
-    newLi.innerHTML = testo;
-    myUl.append(newLi);
+    const box = document.createElement('div');
+    box.innerHTML = testo;
+
+    box.style.width = '100px';
+    box.style.height = '100px';
+    box.style.margin = '10px';
+    box.style.display = 'flex';
+    box.style.alignItems = 'center';
+    box.style.justifyContent = 'center';
+
+    if (className === 'fizzbuzz') {
+        box.style.backgroundColor = '#F0466F';
+    }
+
+    else if (className === 'fizz') {
+        box.style.backgroundColor = '#0CD6A1';
+    }
+
+    else if (className === 'buzz') {
+        box.style.backgroundColor = '#FFD166';
+    }
+
+    else {
+        box.style.backgroundColor = '#1389B2';
+    }
+
+    boxContainer.append(box);
 
 }
